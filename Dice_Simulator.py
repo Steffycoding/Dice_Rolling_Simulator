431a21afa1b7b3dd4da217053b0a33c51622c626
Steffycoding <stephaniepoole2002@gmail.com>
2024-01-17 11:01:50 +0200

    Let's roll the dice

--- Dice_Simulator.py
+++ Dice_Simulator.py
@@ -1,4 +1,11 @@
-Ddef main():
+import random
+import time
+
+def roll_dice():
+    """Generate a random number between 1 and 6."""
+    return random.randint(1, 6)
+
+def main():
     """Main function to run the Dice Rolling Simulator."""
     print("Welcome to the Dice Rolling Simulator!")
     
@@ -9,6 +16,9 @@ Ddef main():
         print(f"You rolled: {result}")
 
         repeat = input("Do you want to roll the dice again? (yes/no): ").lower()
-        if repeat != 'yes':
+        if repeat not in ['yes', 'y']:
             print("Thank you for using the Dice Rolling Simulator. Goodbye!")
             break
+
+if __name__ == "__main__":
+    main()
